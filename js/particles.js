015394b04238d7d89e65cd645c4a23b8a7cbf46e
Ing-Eduardo-E <rekineke@hotmail.com>
2019-09-27 23:27:17 -0500

    Ultimos ajustes animación
    Se elije otra animación y se configura el aspecto con respecto a la página de inicio

--- js/particles.js
+++ js/particles.js
@@ -1505,9 +1505,9 @@ window.particlesJS = function(tag_id, params){
   var canvas_el = document.createElement('canvas');
   canvas_el.className = pJS_canvas_class;
 
-  /* set size canvas */
+  /* set size canvas */ /*permite cambiar el tamaño de la animación*/
   canvas_el.style.width = "100%";
-  canvas_el.style.height = "100%";
+  canvas_el.style.height = "450px";
 
   /* append canvas */
   var canvas = document.getElementById(tag_id).appendChild(canvas_el);
